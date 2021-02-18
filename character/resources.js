1503232322531203e9400e7e4d85a12957b37f13
kayliaray <35606155+kayliaray@users.noreply.github.com>
2021-02-18 14:58:57 -0500

    Add files via upload

---
+++ character/resources.js
@@ -0,0 +1,211 @@
+var races=[
+    {
+      "ID":1,
+      "Race": "A'Erek",
+      "Strength": 12,
+      "Dexterity": 16,
+      "Awareness": 14,
+      "Aptitude": 16,
+      "Fortitude": 12,
+      "Constitution": 14
+    },
+    {"ID":2,
+      "Race": "Bae Fah",
+      "Strength": 20,
+      "Dexterity": 14,
+      "Awareness": 22,
+      "Aptitude": 14,
+      "Fortitude": 10,
+      "Constitution": 14
+    },
+    {
+      "ID":3,
+      "Race": "Dwarf",
+      "Strength": 18,
+      "Dexterity": 10,
+      "Awareness": 10,
+      "Aptitude": 12,
+      "Fortitude": 16,
+      "Constitution": 18
+    },
+    {
+      "ID":3,
+      "Race": "Earthborn",
+      "Strength": 18,
+      "Dexterity": 10,
+      "Awareness": 10,
+      "Aptitude": 12,
+      "Fortitude": 16,
+      "Constitution": 18
+    },
+    {
+      "ID":4,
+      "Race": "Elf",
+      "Strength": 10,
+      "Dexterity": 18,
+      "Awareness": 16,
+      "Aptitude": 16,
+      "Fortitude": 14,
+      "Constitution": 10
+    },
+    {
+      "ID":5,
+      "Race": "Elik",
+      "Strength": 7,
+      "Dexterity": 16,
+      "Awareness": 7,
+      "Aptitude": 18,
+      "Fortitude": 12,
+      "Constitution": 20
+    },
+    {
+      "ID":6,
+      "Race": "Elik Han",
+      "Strength": 18,
+      "Dexterity": 14,
+      "Awareness": 8,
+      "Aptitude": 12,
+      "Fortitude": 5,
+      "Constitution": 20
+    },
+    {
+      "ID":7,
+      "Race": "Entkin",
+      "Strength": 18,
+      "Dexterity": 10,
+      "Awareness": 16,
+      "Aptitude": 12,
+      "Fortitude": 10,
+      "Constitution": 18
+    },
+    {
+      "ID":5,
+      "Race": "Grl-srrrn",
+      "Strength": 10,
+      "Dexterity": 8,
+      "Awareness": 16,
+      "Aptitude": 16,
+      "Fortitude": 20,
+      "Constitution": 20
+    },
+    {
+      "ID":6,
+      "Race": "Harkadian",
+      "Strength": 14,
+      "Dexterity": 14,
+      "Awareness": 14,
+      "Aptitude": 14,
+      "Fortitude": 14,
+      "Constitution": 14
+    },
+    {
+      "ID":7,
+      "Race": "Human",
+      "Strength": 14,
+      "Dexterity": 14,
+      "Awareness": 14,
+      "Aptitude": 14,
+      "Fortitude": 14,
+      "Constitution": 14
+    },
+    {
+      "ID":8,
+      "Race": "Kimtid (Ash Shaytan)",
+      "Strength": 18,
+      "Dexterity": 8,
+      "Awareness": 18,
+      "Aptitude": 16,
+      "Fortitude": 16,
+      "Constitution": 20
+    },
+    {
+      "ID":9,
+      "Race": "Kimtid (Lilith)",
+      "Strength": 12,
+      "Dexterity": 14,
+      "Awareness": 16,
+      "Aptitude": 18,
+      "Fortitude": 18,
+      "Constitution": 18
+    },
+    {
+      "ID":10,
+      "Race": "Kimtid (Mal'Akh)",
+      "Strength": 16,
+      "Dexterity": 16,
+      "Awareness": 18,
+      "Aptitude": 16,
+      "Fortitude": 16,
+      "Constitution": 14
+    },
+    {
+      "ID":11,
+      "Race": "Lizardman",
+      "Strength": 16,
+      "Dexterity": 16,
+      "Awareness": 16,
+      "Aptitude": 12,
+      "Fortitude": 10,
+      "Constitution": 14
+    },
+    {
+      "ID":12,
+      "Race": "Ogre",
+      "Strength": 22,
+      "Dexterity": 10,
+      "Awareness": 10,
+      "Aptitude": 8,
+      "Fortitude": 10,
+      "Constitution": 22
+    },
+    {
+      "ID":13,
+      "Race": "Orc",
+      "Strength": 18,
+      "Dexterity": 10,
+      "Awareness": 18,
+      "Aptitude": 10,
+      "Fortitude": 10,
+      "Constitution": 18
+    },
+    {
+      "ID":14,
+      "Race": "Sgrluk",
+      "Strength": 10,
+      "Dexterity": 24,
+      "Awareness": 14,
+      "Aptitude": 10,
+      "Fortitude": 8,
+      "Constitution": 18
+    },
+    {
+      "ID":15,
+      "Race": "Shabaf",
+      "Strength": 16,
+      "Dexterity": 8,
+      "Awareness": 18,
+      "Aptitude": 12,
+      "Fortitude": 12,
+      "Constitution": 22
+    },
+    {
+      "ID":16,
+      "Race": "Troll",
+      "Strength": 18,
+      "Dexterity": 10,
+      "Awareness": 16,
+      "Aptitude": 12,
+      "Fortitude": 8,
+      "Constitution": 24
+    },
+    {
+      "ID":17,
+      "Race": "Ytharial",
+      "Strength": 4,
+      "Dexterity": 4,
+      "Awareness": 6,
+      "Aptitude": 24,
+      "Fortitude": 24,
+      "Constitution": 26
+    }
+   ]
